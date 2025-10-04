284545b62c5a2793f1fab81426e17eceecd124a0
VirajNaik8055 <naikviraj301@gmail.com>
2025-10-04 20:30:58 +0530

    fixed duplication of variables error

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const pathname = usePathname();
-  const heading = pathname === "/RoleSelectionPage" ? "Dashboard" : "";
+  const _heading = pathname === "/RoleSelectionPage" ? "Dashboard" : "";
 
   return (
     <div className="flex items-center justify-between py-4 px-6 bg-gray-100 shadow-md w-full">
