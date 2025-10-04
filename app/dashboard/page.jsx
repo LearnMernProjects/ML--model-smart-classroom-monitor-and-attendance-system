bded78ef4567118eb59ea8a5fd07ecfb4e21f97f
VirajNaik8055 <naikviraj301@gmail.com>
2025-10-04 20:38:51 +0530

    fixing last issue

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import { motion } from "framer-motion"
+import { motion } from "motion/react"
 
 const Dashboard = () => {
   const [_selectedImage, _setSelectedImage] = useState(null);
